3555a824787a78b4e89fedcfa211f96bf0ad84c7
mariagiaramita07112000 <guarapitamaria@gmail.com>
2021-03-06 16:36:53 +0100

    arreglo de errores

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask , request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from geoalchemy2 import Geometry
 
 app= Flask(__name__)
 
@@ -19,16 +20,19 @@ ma = Marshmallow(app)
 #metodo para definirla
 
 class Task(db.Model):
+
     id = db.Column(db.Integer, primary_key=True )
-    geom= db.Column(db.Float, unique=True )
-    address= db.Column(db.String(70), unique=True)
-    area= db.Column(db.Float, unique=True)
-    rooms= db.Column(db.Float, unique=True)
-    garage = db.Column(db.Float, unique=True)
+    latitud=db.Column(db.Float, nullable=False)
+    longitud=db.Column(db.Float, nullable=False)
+    address= db.Column(db.String(70) ,nullable=False)
+    area= db.Column(db.Float, nullable=False)
+    rooms= db.Column(db.Float, nullable=False)
+    garage = db.Column(db.Float)
 
 #funcion que se ejecuta cada vez que instancio la clase
-    def __init__(self, geom, address, area, rooms, garage):
-        self.geom= geom
+    def __init__(self, latitud, longitud, address, area, rooms, garage):
+        self.latitud= latitud
+        self.longitud=longitud
         self.address= address
         self.area= area
         self.rooms= rooms 
@@ -42,9 +46,9 @@ db.create_all()
 class TaskSchema(ma.Schema):
     class Meta:
         #defino los campos que quiero obtener cada vez que interactue en este schema 
-        fields=('id','geom', 'address', 'area', 'rooms', 'garage')
+        fields=('id','latitud','longitud', 'address', 'area', 'rooms', 'garage')
 
-#creo una variable que me permita lo que me va  a permitir eliminar o crear una tarea pueda interactuar 
+#creo una variable que me permita eliminar o crear una tarea pueda interactuar 
 task_schema= TaskSchema()
 tasks_schema= TaskSchema(many=True)
 
@@ -52,28 +56,29 @@ tasks_schema= TaskSchema(many=True)
 def create_task():
 
     #recibimos la tarea
-    geom= request.json['geom']
+    latitud= request.json['latitud']
+    longitud= request.json['longitud']
     address= request.json['address']
     area= request.json['area']
     rooms = request.json['rooms']
     garage= request.json['garage']
 
     #guardamos la tarea 
-    new_task= Task(geom, address , area , rooms , garage )
+    new_task= Task( latitud, longitud, address , area , rooms , garage )
     db.session.add(new_task)
     db.session.commit()
 
     #desde schema convierto a json 
     #respuesta de la tarea para que el cliente vea lo que ha creado 
-    return task.schema.jsonify(new_task)
+    return task_schema.jsonify(new_task)
 
     #para devolver todas las tareas
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     all_tasks= Task.query.all()
-    result= tasks.schema.dump(all_tasks)
-    return task_schema(jsonify)
+    result= tasks_schema.dump(all_tasks)
+    return jsonify(result)
 
 @app.route('/tasks/<id>', methods=['GET'])
 #para devolver tareas segun el id 
@@ -86,13 +91,15 @@ def get_task(id):
 def update_task(id):
     task = Task.query.get(id)
 
-    geom= request.json['geom']
+    latitud= request.json['latitud']
+    longitud= request.json['longitud']
     address= request.json['address']
     area= request.json['area']
     rooms = request.json['rooms']
     garage= request.json['garage']
 
-    task.geom = geom 
+    task.latitud = latitud
+    task.longitud= longitud
     task.address = address 
     task.area = area
     task.rooms = rooms 
@@ -104,12 +111,15 @@ def update_task(id):
 #para eliminar tareas
 @app.route('/tasks/<id>', methods=['DELETE'])
 def delete_task(id):
-    task= task.query.get(id)
+    task= Task.query.get(id)
     db.session.delete(task)
     db.session.commit()
 
     return task_schema.jsonify(task)
 
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message':'Welcome to my API'})
 
 if __name__=='__main__':
     app.run(debug=True)
